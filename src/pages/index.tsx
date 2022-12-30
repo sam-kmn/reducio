bd3ca79937976653049acc86ad02fd10a92b1ae4
Samuel <91400290+sam-kmn@users.noreply.github.com>
2022-12-30 21:53:29 +0100

    landing animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,14 @@ const Home: NextPage = () => {
   return (
     <Layout>
       <main className="mx-auto flex h-full max-w-lg flex-col items-center justify-start gap-10  pt-32 md:max-w-2xl ">
-        <div className="space-y-2 text-center">
+        <div
+          className="slide-up space-y-2 text-center"
+          style={{ animationDuration: "1s" }}
+        >
           <p className="tracking-widest text-purple-500 md:text-lg">
             DON&apos;T WASTE YOUR TIME
           </p>
-          <h1 className="text-5xl font-medium tracking-wide text-white  md:text-6xl lg:text-7xl ">
+          <h1 className="pb-4 text-5xl font-medium tracking-wide text-white  md:text-6xl lg:text-7xl ">
             Reduce <span className="hidden sm:inline-block">your</span> links
           </h1>
           <h3 className="max-w-2xl text-lg  tracking-wider text-neutral-400 md:text-xl lg:text-2xl">
@@ -18,10 +21,13 @@ const Home: NextPage = () => {
             and store links for easy sharing.
           </h3>
         </div>
-        <div className="flex flex-col items-center justify-center gap-5 text-lg font-medium tracking-wide sm:gap-10 md:flex-row md:gap-14">
+        <div
+          className="slide-up flex flex-col items-center justify-center gap-5 text-lg font-medium tracking-wide sm:gap-10 md:flex-row md:gap-14"
+          style={{ animationDuration: "2s" }}
+        >
           <Link
             href="/dash"
-            className="flex animate-pulse items-center gap-2 text-purple-400"
+            className="flex animate-pulse items-center gap-2 text-purple-400 transition duration-200 hover:scale-105"
           >
             <i className="bi bi-rocket-takeoff"></i>
             Getting started
@@ -29,7 +35,7 @@ const Home: NextPage = () => {
           <Link
             href="https://github.com/sam-kmn"
             passHref={true}
-            className="flex items-center gap-2"
+            className="flex items-center gap-2 transition duration-200 hover:scale-105"
           >
             <i className="bi bi-star"></i>
             Give me star
